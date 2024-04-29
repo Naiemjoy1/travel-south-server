3f757bdb4c3e4d3b7e4d15ef5ca7cf6193165413
Naiemjoy1 <naiemjoy1@gmail.com>
2024-04-29 12:10:52 +0600

    fix vercel file

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const app = express();
 const port = process.env.PORT || 5001;
 
 // middleware
-app.use(cors());
+const corsConfig = {
+  origin: "*",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+};
+app.use(cors(corsConfig));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ccm0dfs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -23,7 +28,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const spotCollection = client.db("travelSpotDB").collection("spot");
 
@@ -114,7 +119,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
